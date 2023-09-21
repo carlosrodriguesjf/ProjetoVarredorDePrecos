e410c45d941a4fa42335f1d899de873fd3b72523
carlosrodriguesjf <carlosrodriguesjf@gmail.com>
2023-09-20 22:22:07 -0300

    Projeto pronto

--- app.py
+++ app.py
@@ -7,79 +7,82 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import datetime as dt
+import schedule
 import os
 
-def iniciar_driver():
-    chrome_options = Options()
-    arguments = ['--lang=pt-BR', '--start-maximized', '--incognito' ]
-    for argument in arguments:
-        chrome_options.add_argument(argument)
 
-    chrome_options.add_experimental_option('prefs', {
-        'download.prompt_for_download': False,
-        'profile.default_content_setting_values.notifications': 2,
-        'profile.default_content_setting_values.automatic_downloads': 1,
+def main():
+    def iniciar_driver():
+        chrome_options = Options()
+        arguments = ['--lang=pt-BR', '--start-maximized', '--incognito' ]
+        for argument in arguments:
+            chrome_options.add_argument(argument)
 
-    })
-    driver = webdriver.Chrome(service=ChromeService(
-        ChromeDriverManager().install()), options=chrome_options)
+        chrome_options.add_experimental_option('prefs', {
+            'download.prompt_for_download': False,
+            'profile.default_content_setting_values.notifications': 2,
+            'profile.default_content_setting_values.automatic_downloads': 1,
 
-    return driver
+        })
+        driver = webdriver.Chrome(service=ChromeService(
+            ChromeDriverManager().install()), options=chrome_options)
 
+        return driver
 
-driver = iniciar_driver()
+    driver = iniciar_driver()
 
-# Acessando o site www.zoom.com.br
-driver.get('https://www.zoom.com.br/search?q=console%20ps5')
+    produtos = ['Console Playstation 5', 'cadeira gamer', 'hd ssd 1tb']
 
-# # Acessando o produto Console Playstation 5 
-# campo_busca = driver.find_element(By.XPATH,'//div/input[@placeholder="Digite sua busca..."]')
-# sleep(1)
-# campo_busca.click()
-# sleep(1)
-# campo_busca.send_keys('Console Playstation 5')
-# sleep(1)
-# campo_busca.send_keys(Keys.ENTER)
-# sleep(1)
+    data = dt.date.today().strftime("%d_%b")
+    hora = dt.datetime.now().strftime('%H_%M')
 
+    nome_pasta = str(data)+"_"+str(hora)
 
-# Selecionando o nome, preço e link de compra dos Consoles Playstation 5
-sleep(2)
-nomes = driver.find_elements(By.XPATH, "//div//div[@class='ProductCard_ProductCard_NameWrapper__lOyZM']//h2")
-precos = driver.find_elements(By.XPATH,"//div//p[@data-testid='product-card::price']")
-links = driver.find_elements(By.XPATH,"//div/a[@class='ProductCard_ProductCard_Inner__tsD4M']")
+    os.mkdir(nome_pasta)
 
-# iniciando contador
-i=0
-data = '04021982'
-nome_arquivo = 'precos_ps5_'+data+'.csv'
-for nome,preco,link in zip(nomes,precos,links):
-    i+=1
-    # Extraindo o link de compra
-    link_processado = link.get_attribute('href')
+    for produto in produtos:
+        # Acessando o site www.zoom.com.br
+        driver.get('https://www.zoom.com.br')
 
-    # Criando arquivo relativo a Console Playstation 5
-    with open(nome_arquivo,'a',encoding='utf-8', newline='') as arquivo:
-        arquivo.write(f' {i} - {nome.text};{preco.text};{link_processado}'+os.linesep) 
+        # Acessando o produto Console Playstation 5 
+        campo_busca = driver.find_element(By.XPATH,'//div/input[@placeholder="Digite sua busca..."]')
+        sleep(1)
+        campo_busca.click()
+        sleep(1)
+        campo_busca.send_keys(produto)
+        sleep(1)
+        campo_busca.send_keys(Keys.ENTER)
+        sleep(1)
 
+        # Selecionando o nome, preço e link de compra dos Consoles Playstation 5
+        sleep(2)
+        nomes = driver.find_elements(By.XPATH, "//div//div[@class='ProductCard_ProductCard_NameWrapper__lOyZM']//h2")
+        precos = driver.find_elements(By.XPATH,"//div//p[@data-testid='product-card::price']")
+        links = driver.find_elements(By.XPATH,"//div/a[@class='ProductCard_ProductCard_Inner__tsD4M']")
 
-# Selecionando o nome, preço e link de compra de cadeira gamer
+        nome_arquivo = produto.lower().replace(' ','_')+'_'+str(data)+'.csv'
 
+        caminho_arquivo = os.path.join(nome_pasta,nome_arquivo)
 
-# Selecionando o nome, preço e link de compra de SSD de 1TB
+        for nome,preco,link in zip(nomes,precos,links):
+            # Extraindo o link de compra
+            link_processado = link.get_attribute('href')
+        
+            # Criando arquivo relativo a Console Playstation 5
+            with open(caminho_arquivo,'a',encoding='utf-8', newline='') as arquivo:
+                arquivo.write(f'{nome.text};{preco.text};{link_processado}'+os.linesep) 
 
-
-
-
-
-driver.quit()
-
-
-# Salvar todos os dados em uma planilha
+    driver.quit()
+    print('Fim da Execução')
 
 
+# Agendando a execução do programa para todo dia 0:00 h
+schedule.every().day.at('00:00').do(main)
 
-# Agendar a execução do programa para todo dia 0:00 h
+while True:
+    schedule.run_pending()
+    sleep(1)
 
 
 
